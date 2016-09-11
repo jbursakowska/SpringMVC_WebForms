9335274eaa97910e86579c2f52c83ec9f260d529
Joanna Bursakowska <jbursak@gmail.com>
2016-09-11 14:49:52 -0500

    Completed form submit logic

--- src/main/java/com/yolonerds/web/MainController.java
+++ src/main/java/com/yolonerds/web/MainController.java
@@ -1,9 +1,6 @@
 package com.yolonerds.web;
  
 import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.servlet.ModelAndView;
-
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -12,14 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MainController {
- 
-	/*@RequestMapping("/welcome")
-	public ModelAndView helloWorld() {
- 
-		String message = "Subscribe to BowHunting Magazine";
-		return new ModelAndView("welcome", "message", message);
-	}*/
-	
+ 	
 	@GetMapping("/form")
 	public String getForm(Model model) {
  
