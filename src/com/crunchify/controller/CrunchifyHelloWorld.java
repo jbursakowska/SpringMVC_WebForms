aab6e7488e5e4abb3e05ed013ad737c8485a2183
Joanna Bursakowska <jbursak@gmail.com>
2016-09-10 23:25:57 -0500

    Updated welcome.jsp

--- src/com/crunchify/controller/CrunchifyHelloWorld.java
+++ src/com/crunchify/controller/CrunchifyHelloWorld.java
@@ -15,8 +15,7 @@ public class CrunchifyHelloWorld {
 	@RequestMapping("/welcome")
 	public ModelAndView helloWorld() {
  
-		String message = "<br><div style='text-align:center;'>"
-				+ "<h3>********** Hello World, Spring MVC Tutorial</h3>This message is coming from CrunchifyHelloWorld.java **********</div><br><br>";
+		String message = "Subscribe to BowHunting Magazine";
 		return new ModelAndView("welcome", "message", message);
 	}
 }
