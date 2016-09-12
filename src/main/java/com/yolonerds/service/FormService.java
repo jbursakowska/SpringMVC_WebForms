e0a4e56fa9e1731b75971ca5b6e70641f0add659
Joanna Bursakowska <jbursak@gmail.com>
2016-09-11 22:42:11 -0500

    added models, services, dependencies, and folders

---
+++ src/main/java/com/yolonerds/service/FormService.java
@@ -0,0 +1,40 @@
+package com.yolonerds.service;
+
+import java.util.List;
+
+import com.yolonerds.model.DBFormQuestionModel;
+import com.yolonerds.model.DBSubmissionModel;
+import com.yolonerds.model.WebFormModel;
+
+public class FormService implements IFormService {
+
+	public FormService() {
+		
+	}
+
+	public void submitForm(WebFormModel model) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public DBSubmissionModel createDBSubmissionModel(WebFormModel model) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public DBSubmissionModel insertDBSubmissionModel(DBSubmissionModel model) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<DBFormQuestionModel> createDBFormQuestionModels(WebFormModel model) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void insertDBFormQuestionModels(List<DBFormQuestionModel> list) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
--- src/main/java/com/yolonerds/web/FormController.java
+++ src/main/java/com/yolonerds/web/FormController.java
@@ -5,21 +5,37 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import com.yolonerds.model.WebFormModel;
+import com.yolonerds.service.FormService;
  
 
 @Controller
-public class MainController {
+public class FormController {
+	
+	private FormService service;
+	
+	public FormController(){
+		service = new FormService();
+	}
  	
+	// send html form to the browser
+	
 	@GetMapping("/form")
 	public String getForm(Model model) {
  
-		model.addAttribute("formModel", new FormModel());
+		model.addAttribute("formModel", new WebFormModel());
+		
 		return "form";
 	}
 	
+	// accept data from the submitted form
+	
 	@PostMapping("/form")
-	public String postForm(@ModelAttribute FormModel formModel) {
-
+	public String postForm(@ModelAttribute WebFormModel model) {
+		
+		service.submitForm(model);
+		
 		return "result";
 	}
 }
